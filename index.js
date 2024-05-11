76db79d85a6252882ba1b3d298ec486c64222d6f
Alexey <alexey.anasov@yandex.by>
2024-05-11 21:48:03 +0300

    JS выпадающий список + верстка и стилизация корзины

--- index.js
+++ index.js
@@ -1,3 +1,84 @@
 import "@/scss/index.scss";
 
-console.log("hellp");
+const header = document.querySelector(".header");
+const body = document.body;
+let headerHeight = header.offsetHeight;
+
+window.addEventListener("resize", () => {
+  headerHeight = header.offsetHeight;
+});
+
+window.addEventListener("scroll", () => {
+  const scrollDistance = window.scrollY;
+
+  if (scrollDistance > 200) {
+    header.classList.add("header_fixed");
+    body.style.paddingTop = `${headerHeight}px`;
+  } else {
+    header.classList.remove("header_fixed");
+    body.style.paddingTop = "0";
+  }
+});
+
+const adjustElementPosition = (element, count = 0) => {
+  const rect = element.getBoundingRect();
+  const viewportWidth = window.innerWidth;
+
+  if (rect.left < 0) {
+    element.style.left = "0";
+    element.style.right = "auto";
+    element.style.transform = "translateX(0)";
+  } else if (rect.right > viewportWidth) {
+    element.style.left = "auto";
+    element.style.right = "0";
+    element.style.transform = "translateX(0)";
+  } else {
+    element.style.left = "50%";
+    element.style.right = "auto";
+    element.style.transform = "translateX(-50%)";
+  }
+
+  const postRect = element.getBoundingRect();
+
+  if ((postRect.left < 0 || postRect.right > viewportWidth) && count < 3) {
+    count++;
+    adjustElementPosition(element, count);
+  }
+};
+
+const choices = document.querySelectorAll(".choices");
+
+choices.forEach((choice) => {
+  const btn = choice.querySelector(".choices__btn");
+  const box = choice.querySelector(".choices__box");
+
+  btn.addEventListener("click", () => {
+    box.classList.toggle("choices__box_open");
+
+    choices.forEach((otherChoice) => {
+      if (otherChoice !== choice) {
+        otherChoice
+          .querySelector(".choices__box")
+          .classList.remove("choices__box_open");
+      }
+    });
+
+    adjustElementPosition(box);
+  });
+
+  window.addEventListener("resize", () => {
+    adjustElementPosition(box);
+  });
+});
+
+const headerCartButton = document.querySelector(".header__cart-button");
+const cartClose = document.querySelector(".cart__close");
+const cart = document.querySelector(".cart");
+
+headerCartButton.addEventListener("click", () => {
+  cart.classList.toggle("cart_open");
+});
+
+cartClose.addEventListener("click", () => {
+  cart.classList.remove("cart_open");
+});
