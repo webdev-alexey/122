8b3848ddf50cfc9fa7556853825861646c5a1bd2
Alexey <alexey.anasov@yandex.by>
2024-05-14 14:59:45 +0300

    добавлен search, preload, фильтрация

---
+++ src/scripts/searchProducts.js
@@ -0,0 +1,23 @@
+import { fetchProducts } from "./API";
+import { callBackhWithPreload } from "./preload";
+
+export const initSearchProducts = () => {
+  const headerForm = document.querySelector(".header__form");
+  const goodsSection = document.querySelector(".goods");
+  const goodsTitle = document.querySelector(".goods__title");
+
+  headerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(headerForm);
+
+    const searchQuery = formData.get("search").trim();
+
+    if (searchQuery) {
+      goodsTitle.textContent = "Результат поиска";
+      callBackhWithPreload(goodsSection, fetchProducts, {
+        search: searchQuery,
+      });
+    }
+  });
+};
